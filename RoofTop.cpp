f21d841cc740789b31f7ee9c87ac74ccdab16a79
PRABAL4546 <prabaljitwalia4546@gmail.com>
2021-04-22 18:13:05 +0530

    bsearch

---
+++ RoofTop.cpp
@@ -0,0 +1,36 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+class Solution{
+    public:
+        int maxStep(int A[], int N){
+        
+       //Your code here
+    }
+};
+
+// { Driver Code Starts.
+
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    
+	    int a[n];
+	    
+	    for(int i=0;i<n;i++)
+	    cin>>a[i];
+	    Solution ob;
+	    cout << ob.maxStep(a, n) << endl;
+	}
+	return 0;
+}  // } Driver Code Ends
