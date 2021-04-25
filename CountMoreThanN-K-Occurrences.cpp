d0c62f127806006e833f7ade3d84ad1dd2c72c51
PRABAL4546 <prabaljitwalia4546@gmail.com>
2021-04-25 22:57:24 +0530

    bSearch Prob

---
+++ CountMoreThanN-K-Occurrences.cpp
@@ -0,0 +1,36 @@
+// { Driver Code Starts
+// A C++ program to print elements with count more than n/k
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+// Function to find elements with count more than n/k times
+// arr: input array
+class Solution{
+  public:
+    int countOccurence(int arr[], int n, int k) {
+        // Your code here
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t, k;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+
+        int arr[n];
+
+        for (i = 0; i < n; i++) cin >> arr[i];
+        cin >> k;
+        Solution obj;
+        cout << obj.countOccurence(arr, n, k) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
