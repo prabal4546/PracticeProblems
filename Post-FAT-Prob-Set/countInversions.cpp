492f112ebe6e651e758b27afac5825c0e0d35f49
PRABAL4546 <prabaljitwalia4546@gmail.com>
2021-06-12 10:59:26 +0530

    sorting

--- Post-FAT-Prob-Set/countInversions.cpp
+++ Post-FAT-Prob-Set/countInversions.cpp
@@ -1,1 +1,51 @@
-//post fat exam problem set
+// { Driver Code Starts
+//Initial Template for C
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+
+// arr[]: Input Array
+// N : Size of the Array arr[]
+// Function to count inversions in the array.
+long long int inversionCount(long long arr[], long long N)
+{
+    // Your Code Here
+    int res=0;
+    for(int i=0;i<N;i++){
+        for(int j=i+1;j<N;j++){
+            if (arr[i]>arr[j]){
+                res++;
+            }
+        }
+    }
+    return res;
+}
+
+
+
+// { Driver Code Starts.
+
+int main() {
+    
+    long long T;
+    scanf("%ld", &T);
+    
+    while(T--){
+        long long N;
+        scanf("%ld", &N);
+        
+        long long A[N];
+        for(long long i = 0;i<N;i++){
+            scanf("%ld", &A[i]);
+        }
+        
+        printf("%ld\n", inversionCount(A,N) );
+    }
+    
+    return 0;
+}
