ea0d0290ec726eb152cf1085902772e3d09d166f
PRABAL4546 <prabaljitwalia4546@gmail.com>
2021-06-14 15:32:36 +0530

    Sort by Absolute Difference

---
+++ Post-FAT-Prob-Set/Sort by Absolute Difference.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+class Solution{
+    public:
+    
+    // A[]: input array
+    // N: size of array
+    //Function to sort the array according to difference with given number.
+    void sortABS(int A[],int N, int k)
+    {
+       //Your code here
+    }
+
+};
+
+// { Driver Code Starts.
+
+int main()
+ {
+	int T;
+	cin>> T;
+	
+	while (T--)
+	{
+	    
+	    int N, diff;
+	    cin>>N>>diff;
+	    int A[N];
+	    
+	    for(int i = 0; i<N; i++)
+	        cin>>A[i];
+	   
+	    Solution ob;
+	   
+	    ob.sortABS(A, N, diff);
+	    
+	    for(int & val : A)
+	        cout<<val<<" ";
+	    cout<<endl;
+	    
+	}
+	
+	return 0;
+}  // } Driver Code Ends
