df941bead188c58036a4c71dea096b54c6f1e25f
PRABAL4546 <prabaljitwalia4546@gmail.com>
2021-06-18 23:27:41 +0530

    wave array

---
+++ Post-FAT-Prob-Set/Wave Array.cpp
@@ -0,0 +1,39 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to sort the array into a wave-like array.
+    void convertToWave(int *arr, int n){
+        
+        // Your code here
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    cin>>t; //Input testcases
+    while(t--) //While testcases exist
+    {
+        cin>>n; //input size of array
+        int a[n]; //declare array of size n
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //input elements of array
+        Solution ob;
+        ob.convertToWave(a, n);
+
+        for(int i=0;i<n;i++)
+            cout<<a[i]<<" "; //print array
+            
+        cout<<endl;
+    }
+}
