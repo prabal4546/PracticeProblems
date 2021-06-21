2011f048279edd33f781e9a7f6940c6968e9934b
PRABAL4546 <prabaljitwalia4546@gmail.com>
2021-06-21 21:11:00 +0530

    Power Set Using Recursion

---
+++ Post-FAT-Prob-Set/Power Set Using Recursion.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+
+//Function to return the lexicographically sorted power-set of the string.
+vector <string> powerSet(string s)
+{
+   //Your code here
+}
+
+
+// { Driver Code Starts.
+
+
+// Driver code
+int main()
+{
+    int T;
+    cin>>T;//testcases
+    while(T--)
+    {
+        string s;
+        cin>>s;//input string
+        
+        //calling powerSet() function
+        vector<string> ans = powerSet(s);
+        
+        //sorting the result of the powerSet() function
+        sort(ans.begin(),ans.end());
+        
+        //printing the result
+        for(auto x:ans)
+        cout<<x<<" ";
+        cout<<endl;
+        
+        
+    }
+
+return 0;
+}
