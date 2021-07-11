270c29e9f4c46f4f763ea309f76e36d4fc7c5034
prabal4546 <prabaljitwalia4546@gmail.com>
2021-07-11 19:48:06 +0530

    driver code

---
+++ Post-FAT-Prob-Set/countPossibleTriangles.cpp
@@ -0,0 +1,35 @@
+/ { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to count the number of possible triangles.
+    int findNumberOfTriangles(int arr[], int n)
+    {
+        // code here
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0; i<n; i++)
+            cin>>arr[i];
+        Solution ob;
+        cout<<ob.findNumberOfTriangles(arr,n) <<endl;
+    }
+    return 0;
+} 
