66dd8033d9e9df64c01d73ac1c35f4d3e8d70a92
prabal4546 <prabaljitwalia4546@gmail.com>
2021-09-20 00:38:48 +0530

    deleted redundant problem

--- CSE1007/Simple.java
+++ CSE1007/Simple.java
@@ -14,17 +14,3 @@ public class Main
     }    
 }
 }
-// multiplication table 
-public class Main
-{
-	public static void main(String[] args) {
-	    Scanner sc = new Scanner(System.in);
-	    System.out.print("Enter number: "); 
-	    int a= sc.nextInt(); 
-	    int p=1;
-	    for(int i=1;i<=10;i++){
-	         p = a*(i);
-	         System.out.println(a+"*"+(i)+"="+p);
-	    }	        
-	}
-}
